705d8e588cd55f0148f30d9bafe0ca283b681c5c
Julio Santos <julioc.9730@gmail.com>
2022-10-20 15:03:46 -0300

    Create CartPages.java

---
+++ src/test/java/pages/CartPages.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class CartPages {
+
+    @FindBy(css = "input[placeholder='Digite a sua busca']")
+    protected WebElement fldSearch;
+
+    @FindBy(css = "a[class='bf-shelf-item__container']")
+    protected List<WebElement> tltProduct;
+
+    @FindBy(xpath = "(//*[contains(@class,'radio-item-')])[3]")
+    protected WebElement fldSize;
+
+    @FindBy(css = "td[class='product-name']")
+    protected List<WebElement> fdlProduct;
+
+    @FindBy(css = "#cart-title")
+    protected WebElement fldMyCart;
+}
