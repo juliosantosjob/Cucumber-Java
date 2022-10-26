e9dfa3d25f37181b4ab2a69a0aff1113601e8cdc
Julio Santos <julioc.9730@gmail.com>
2022-10-26 18:13:07 -0300

    Update DriverFactory.java

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -2,64 +2,45 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import static utils.PropertiesFile.getProperty;
 
 import java.time.Duration;
 
 public class DriverFactory {
     protected static WebDriver driver;
+    String Browser = System.getProperty("Browser");
 
     public void setDriver() {
-        switch (System.getProperty("Browser")) {
+        switch (Browser) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
-
             case "chrome-headless":
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--headless");
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver(options);
                 break;
-
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
-
             case "firefox-headless":
                 WebDriverManager.firefoxdriver().setup();
-                FirefoxBinary firefoxBinary = new FirefoxBinary();
                 FirefoxOptions optionsFox = new FirefoxOptions();
-                optionsFox.setBinary(firefoxBinary);
                 optionsFox.setHeadless(true);
                 driver = new FirefoxDriver(optionsFox);
                 break;
-
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-
-            case "edge-headless":
-                WebDriverManager.edgedriver().setup();
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless");
-                driver = new EdgeDriver(edgeOptions);
-                break;
-
             default:
                 System.out.println("Non-existent browser");
         }
-
-        String BaseUrl = "https://www.estrelalojas.com.br";
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get(BaseUrl);
